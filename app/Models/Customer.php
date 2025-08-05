ac73c73b364c201db43222b984757dace6413c50
Ahmed Hesham <ahmed.hesham@eden-tech.io>
2025-08-05 16:50:49 +0300

    init laravel project

---
+++ app/Models/Customer.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    protected $table = 'customer';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'name', 'phone'
+    ];
+}
